b0981a2b0ccf9b24fe17e78e3da0bd59e2d74794
youssefoubens <oubenssaidyoussef@gmail.com>
2025-09-30 14:37:23 +0100

    creation of the project

---
+++ src/main/java/com/youssef/event_driven_kafka/EventDrivenKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.event_driven_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youssef/event_driven_kafka/EventDrivenKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.event_driven_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
