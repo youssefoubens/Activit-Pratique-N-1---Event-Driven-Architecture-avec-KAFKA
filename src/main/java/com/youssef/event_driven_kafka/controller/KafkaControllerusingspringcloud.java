398c4cacecfa1a94cccbe59a5bbc1831154a08c9
youssefoubens <oubenssaidyoussef@gmail.com>
2025-10-05 19:01:05 +0100

    update Readme file

--- src/main/java/com/youssef/event_driven_kafka/controller/KafkaControllerusingspringcloud.java
+++ src/main/java/com/youssef/event_driven_kafka/controller/KafkaControllerusingspringcloud.java
@@ -1,13 +1,27 @@
 package com.youssef.event_driven_kafka.controller;
 
 import com.youssef.event_driven_kafka.entities.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 
@@ -16,6 +30,8 @@ public class KafkaControllerusingspringcloud {
 
     @Autowired
     private StreamBridge streamBridge;
+    @Autowired
+    private InteractiveQueryService interactiveQueryService;
 
     @GetMapping("/publish/{topic}/{name}")
     public PageEvent publish(@PathVariable String topic,
@@ -25,8 +41,31 @@ public class KafkaControllerusingspringcloud {
                 PageEvent(topic+"+"+name,Math.random()>0.5?"U1":"U2",new Date(),new
                 Random().nextInt(9000));
 
-        streamBridge.send("pageEventProducer-out-0",pageEvent);
+        Message<PageEvent> message = MessageBuilder
+                .withPayload(pageEvent)
+                .setHeader("kafka_messageKey", pageEvent.getName())
+                .build();
+        System.out.println(message);
+        streamBridge.send("pageEventProducer-out-0",message);
         return pageEvent;
+    }
+
+    @GetMapping(path = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Map<String, Long>> analytics(){
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence->{
+                    Map<String,Long> stringLongMap=new HashMap<>();
+                    ReadOnlyWindowStore<String, Long> windowStore = interactiveQueryService.getQueryableStore("count-store", QueryableStoreTypes.windowStore());
+                    Instant now=Instant.now();
+                    Instant from=now.minusMillis(5000);
+                    KeyValueIterator<Windowed<String>, Long> fetchAll = windowStore.fetchAll(from, now);
+                    //WindowStoreIterator<Long> fetchAll = windowStore.fetch(page, from, now);
+                    while (fetchAll.hasNext()){
+                        KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                        stringLongMap.put(next.key.key(),next.value);
+                    }
+                    return stringLongMap;
+                });
     }
 
 
--- src/main/java/com/youssef/event_driven_kafka/service/KafkaConsumerVersion2.java
+++ src/main/java/com/youssef/event_driven_kafka/service/KafkaConsumerVersion2.java
@@ -2,21 +2,62 @@ package com.youssef.event_driven_kafka.service;
 
 
 import com.youssef.event_driven_kafka.entities.PageEvent;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.function.Consumer;
+import java.util.function.Function;
+
 
 @Service
+@EnableScheduling
 public class KafkaConsumerVersion2 {
 
 
+    @Autowired
+    private StreamBridge streamBridge;
     @Bean
-    public Consumer<PageEvent> pageEventConsumer(){
-        return (input)->{
+    public Consumer<PageEvent> pageEventConsumer() {
+        return (input) -> {
             System.out.println("**********************");
             System.out.println(input.toString());
             System.out.println("**********************");
         };
+    }
 
-}}
+
+
+//    @Scheduled(fixedDelay = 1000)
+//    public void publishPageEvent() {
+//        PageEvent pageEvent = new PageEvent(
+//                "youssef" + Math.random() * 10,
+//                Math.random() * 10 > 5 ? "U1" : "U2",
+//                new Date(),
+//                (int) (Math.random() * 100)
+//        );
+//        streamBridge.send("pageEventProducer-out-0", pageEvent);
+//        System.out.println("Published: " + pageEvent);
+//    }
+
+    @Bean
+    Function<KStream<String, PageEvent>, KStream<String, PageEvent>> pageEventKStream() {
+        return (input) -> {
+            input.peek((key, value) -> {
+                System.out.println("********************** from KStream function");
+                System.out.println("Key = " + key + ", Value = " + value);
+                System.out.println("**********************");
+            });
+            return input;
+        };
+    }
+
+
+
+}
